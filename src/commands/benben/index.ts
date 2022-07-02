febeec616580e993dbffb8e761be63824c790f6e
王品呈 <wpcwzy@gmail.com>
2022-07-02 12:33:55 +0800

    fix #42

--- src/commands/benben/index.ts
+++ src/commands/benben/index.ts
@@ -147,7 +147,7 @@ export default new SuperCommand({
         class="am-comment-avatar"/></a></div><div class="am-comment-main"><header class="am-comment-hd"><div class="am-comment-meta"><span class="feed-username"><a class='${tmp[i][1]}' href="https://www.luogu.com.cn/user/${tmp[i][0]}"  target="_blank">${tmp[i][2]}</a>${tmp[i][3]}</span> ${tmp[i][4]}<a name="feed-reply" href="javascript: scrollToId('feed-content')" data-username="${tmp[i][2]}">回复</a></div></header><div class="am-comment-bd"><span class="feed-comment">${tmp[i][6]}</span></div></div></li>`);
         }
         pannel.webview.postMessage({ type: 'benbennew', message: message });
-        retryTimes = 0
+        retryTimes = maxRetryTimes + 2 //max+1说明重试次数超限引起错误（下面有if判断），max+2则说明正常结束
       } catch (err) {
         console.error(err)
         vscode.window.showErrorMessage(`获取犇犇失败，已重试 ${retryTimes} 次`);
