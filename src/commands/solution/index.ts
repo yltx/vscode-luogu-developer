7627fe674f659f1a62788ed4c767287f27a4293c
gandyli <andyli20070803@gmail.com>
2024-02-02 16:16:36 +0800

    Fix background color in solution index.ts

--- src/commands/solution/index.ts
+++ src/commands/solution/index.ts
@@ -255,7 +255,7 @@ const genSolutionHTML = async function (
       left: 0;
       right: 0;
       border-top: 1px solid #e0e0e0;
-      background-color: white;
+      # background-color: white;
       padding:5px;
     }
     #vote{
